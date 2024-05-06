d4369d6ee0587430e1e34b6bec3e7958a9502f3d
leestana01 <leestana01@naver.com>
2024-05-06 17:18:40 +0900

    [INIT] 초기 설정 완료

---
+++ src/main/java/com/likelion/mini2team/Mini2teamApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.mini2team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mini2teamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mini2teamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/mini2team/Mini2teamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.mini2team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mini2teamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
