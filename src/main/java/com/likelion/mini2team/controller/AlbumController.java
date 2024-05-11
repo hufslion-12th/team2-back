67b1bc4e681e23a94cbba19127829a9d380b3a79
ryu <ryudonghyun4918@naver.com>
2024-05-12 01:27:57 +0900

    [ADD] 앨범 프로그램 추가

--- src/main/java/com/likelion/mini2team/controller/AlbumController.java
+++ src/main/java/com/likelion/mini2team/controller/AlbumController.java
@@ -1,7 +1,36 @@
 package com.likelion.mini2team.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.likelion.mini2team.domain.album.Album;
+import com.likelion.mini2team.service.AlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/albums")
 public class AlbumController {
+
+    @Autowired
+    private AlbumService albumService;
+
+    @PostMapping
+    public Album addAlbum(@RequestParam String name) {
+        return albumService.addAlbum(name);
+    }
+
+    @GetMapping
+    public List<Album> getAllAlbums() {
+        return albumService.getAllAlbums();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAlbum(@PathVariable Long id) {
+        albumService.deleteAlbum(id);
+    }
+
+    @PutMapping("/{id}")
+    public Album updateAlbumName(@PathVariable Long id, @RequestParam String newName) {
+        return albumService.updateAlbumName(id, newName);
+    }
 }
--- src/main/java/com/likelion/mini2team/service/AlbumService.java
+++ src/main/java/com/likelion/mini2team/service/AlbumService.java
@@ -1,7 +1,45 @@
 package com.likelion.mini2team.service;
 
+import com.likelion.mini2team.domain.album.Album;
+import com.likelion.mini2team.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AlbumService {
+
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    /* 앨범 추가*/
+    public Album addAlbum(String name) {
+        Album album = new Album();
+        album.setName(name);
+        return albumRepository.save(album);
+    }
+
+    /* 앨범 조회*/
+    public List<Album> getAllAlbums() {
+        return albumRepository.findAll();
+    }
+
+    /* 앨범 삭제 */
+    public void deleteAlbum(Long id) {
+        albumRepository.deleteById(id);
+    }
+
+    /* 앨범 수정 */
+    public Album updateAlbumName(Long id, String newName) {
+        Optional<Album> optionalAlbum = albumRepository.findById(id);
+        if (optionalAlbum.isPresent()) {
+            Album album = optionalAlbum.get();
+            album.setName(newName);
+            return albumRepository.save(album);
+        } else {
+            throw new IllegalArgumentException("해당 ID의 앨범을 찾을 수 없습니다.");
+        }
+    }
 }
